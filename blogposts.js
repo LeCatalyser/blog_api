03731789b1e3e0caa2fc96805cc7c88e1063e2b3
Luisa Salcedo <profe.salcedo@gmail.com>
2017-05-16 19:50:29 -0500

    complteed post, delete and put code

--- blogposts.js
+++ blogposts.js
@@ -28,8 +28,46 @@ router.post('/', jsonParser, (req, res) => {
 }
 	const item = blogposts.create(
 		req.body.title, req.body.content, req.body.author);
+	//why is body repeated per title, content and author?
 	res.status(201).json(item);
 });
+
+router.put('/:id', jsonParser (req, res) => {
+	const requiredFields = [
+		'id', 'title', 'content', 'author', 'publishDate'];
+		for (let i=0; i,requiredFields.length; i++) {
+			const field = requiredFields[i];
+			if(!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`
+			console.error(message);
+			return res.status(400).send(message);
+		}
+		if (req.params.id ! == req.body.id) {
+			const message = (
+				`Request path id (${req.params.id}) and request body id`
+				`(${req.body.id}) must match`);
+			console.error (message);
+			return res.status(400).send(message);
+		}
+
+		console.log (`updating blog post with id \`${req.params.id}\``);
+			const updatedItem = blogposts.update({
+				id: req.params.id,
+				title: req.body.title,
+				content: req.body.content,
+				author: req.body.author,
+				publishDate: req.body.publishDate
+			});
+			res.status(204).json(updatedItem);
+});
+
+router.delete('/:id', (req,res) => {
+	blogposts.delete(req.params.id);
+	console.log(`Deleted blog post with id \`${req.params.ID}\``);
+	res.status(204).end();
+});
+
+module.exports = router;
 
 
 
